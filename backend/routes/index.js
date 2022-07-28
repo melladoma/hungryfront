b636adf1843003e230662984cb8cfca9072e11f0
Marine Mellado <marine.mellado@gmail.com>
2022-07-28 18:14:10 +0200

    essai config tesseract

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,11 +9,38 @@ const vision = require("@google-cloud/vision");
 
 //Test tesseract
 router.get("/tesseract", async function (req, res, next) {
-	var text = await tesseract.recognize(
-		"https://picturetherecipe.com/wp-content/uploads/2013/07/Picture-The-Recipe-Tips-Muffin-tin-for-stuffed-veggies.jpg"
-	);
+	// const config = {
+	// 	lang: "eng",
+	// 	oem: 1,
+	// 	psm: 3,
+	// }
+	// var text = await tesseract.recognize(
+	// 	"https://picturetherecipe.com/wp-content/uploads/2013/07/Picture-The-Recipe-Tips-Muffin-tin-for-stuffed-veggies.jpg", config
+	// );
 
-	res.json({ text });
+	// var text = await tesseract.recognize(
+	// 	"https://picturetherecipe.com/wp-content/uploads/2013/07/Picture-The-Recipe-Tips-Muffin-tin-for-stuffed-veggies.jpg"
+	// );
+
+
+	const config = {
+		lang: "eng",
+		oem: 1,
+		psm: 4,
+	}
+
+	tesseract
+		.recognize("https://picturetherecipe.com/wp-content/uploads/2013/07/Picture-The-Recipe-Tips-Muffin-tin-for-stuffed-veggies.jpg", config)
+		.then((text) => {
+			console.log("Result:", text)
+			res.json({ text });
+		})
+		.catch((error) => {
+			console.log(error.message)
+			res.json({ message: "err" });
+		})
+	// res.json({ text });
+
 });
 
 //Test Cloud Vision
@@ -28,9 +55,9 @@ router.get("/cloud-vision", async function (req, res, next) {
 	);
 	const detections = result.textAnnotations;
 
-  const justeLeTexte = detections[0].description
+	const justeLeTexte = detections[0].description
 	console.log("Text:", justeLeTexte);
-	
+
 
 	res.json({ justeLeTexte });
 });
@@ -43,29 +70,29 @@ router.get("/cloud-vision-doc", async function (req, res, next) {
 
 	// Performs label detection on the image file
 	const [result] = await client.documentTextDetection("https://picturetherecipe.com/wp-content/uploads/2013/07/Picture-The-Recipe-Tips-Muffin-tin-for-stuffed-veggies.jpg");
-const fullTextAnnotation = result.fullTextAnnotation;
+	const fullTextAnnotation = result.fullTextAnnotation;
 
-const justeLeTexte = fullTextAnnotation.text
+	const justeLeTexte = fullTextAnnotation.text
 
 
-console.log(`Full text: ${justeLeTexte}`);
-/* fullTextAnnotation.pages.forEach(page => {
-  page.blocks.forEach(block => {
-    console.log(`Block confidence: ${block.confidence}`);
-    block.paragraphs.forEach(paragraph => {
-      console.log(`Paragraph confidence: ${paragraph.confidence}`);
-      paragraph.words.forEach(word => {
-        const wordText = word.symbols.map(s => s.text).join('');
-        console.log(`Word text: ${wordText}`);
-        console.log(`Word confidence: ${word.confidence}`);
-        word.symbols.forEach(symbol => {
-          console.log(`Symbol text: ${symbol.text}`);
-          console.log(`Symbol confidence: ${symbol.confidence}`);
-        });
-      });
-    });
-  });
-}); */
+	console.log(`Full text: ${justeLeTexte}`);
+	/* fullTextAnnotation.pages.forEach(page => {
+	  page.blocks.forEach(block => {
+		console.log(`Block confidence: ${block.confidence}`);
+		block.paragraphs.forEach(paragraph => {
+		  console.log(`Paragraph confidence: ${paragraph.confidence}`);
+		  paragraph.words.forEach(word => {
+			const wordText = word.symbols.map(s => s.text).join('');
+			console.log(`Word text: ${wordText}`);
+			console.log(`Word confidence: ${word.confidence}`);
+			word.symbols.forEach(symbol => {
+			  console.log(`Symbol text: ${symbol.text}`);
+			  console.log(`Symbol confidence: ${symbol.confidence}`);
+			});
+		  });
+		});
+	  });
+	}); */
 
 	res.json({ justeLeTexte });
 });
