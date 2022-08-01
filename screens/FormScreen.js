b407d7bfb3c10e7d159737fbe7a6c5d8b5e00d3e
Marine Mellado <marine.mellado@gmail.com>
2022-08-01 11:06:14 +0200

    formulaire mis en place, sans inputs multiples ou toggle

--- screens/FormScreen.js
+++ screens/FormScreen.js
@@ -11,15 +11,44 @@ import {
 	Button,
 	TouchableOpacity,
 	Text,
+	TextInput,
+	ScrollView
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { MaterialCommunityIcons } from "react-native-vector-icons";
 
+
 function FormScreen(props) {
 	const navigation = useNavigation();
+	var handleSubmitForm = () => {
+		navigation.navigate("RecipeSheetScreen")
+	}
 
-	const [searchInput, setSearchInput] = useState("");
+
+	//comment gerer multiple inputs in react native?
+	const [recipeTags, setRecipeTags] = useState("");
+	const [recipeIngredients, setRecipeIngredients] = useState("");
+
+	const [recipe, setRecipe] = useState({ name: "", image: "", prepTime: "", cookTime: "", author: "", direction: "", privateStatus: "", tags: [], ingredients: [] })
+
+	// const [ingredientsFormInputs, setIngredientsFormInputs] = React.useState([
+	// 	{ key: "", name: "", quantity: "" },
+	// 	{ key: "", name: "", quantity: "" },
+	// ]);
+
+	// //Add more inputs rows
+	// const handleAddMoreLine = () => {
+	// 	const _inputs = [...ingredientsFormInputs];
+	// 	_inputs.push({ key: "", name: "", quantity: "" });
+	// 	setIngredientsFormInputs(_inputs);
+	// }
+	// const inputHandleName = (text, key) => {
+	// 	const _formNameInputs = [...ingredientsFormInputs];
+	// 	_formNameInputs[key].key = key;
+	// 	_formNameInputs[key].name = value;
+	// 	setIngredientsFormInputs(_formNameInputs);
+	// }
 
 	//----------------------------- ------------------------------------Début StatusBar
 	const MyStatusBar = ({ backgroundColor, ...props }) => (
@@ -34,18 +63,101 @@ function FormScreen(props) {
 			</SafeAreaView>
 		</View>
 	);
-	//----------------------------- ------------------------------------Fin de StatusBar
+	//-----------------------------------------------------------------Fin de StatusBar
 
 	return (
 		<View style={styles.container}>
 			<MyStatusBar backgroundColor="#dfe4ea" barStyle="dark-content" />
-
-			<View style={styles.content}>
-				<Text style={{ fontSize: 20 }}>FormScreen</Text>
-				<Button
-					title="Valider le formulaire"
-					onPress={() => navigation.navigate("RecipeSheetScreen")}
+			{/* ------------------------------------Debut du formulaire */}
+			<ScrollView style={{ flex: 1 }}>
+				<Text style={styles.label}>Nom de la recette</Text>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setRecipeName(value)}
+					value={recipe.name}
 				/>
+
+				<Text style={styles.label}>Image</Text>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setRecipeImage(value)}
+					value={recipe.image}
+				/>
+				<Text style={styles.label}>Temps de préparation</Text>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setPrepTime(value)}
+					value={recipe.prepTime}
+				/>
+
+				<Text style={styles.label}>Temps de cuisson</Text>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setCookTime(value)}
+					value={recipe.cookTime}
+				/>
+
+				<Text style={styles.label}>Auteur</Text>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setAuthor(value)}
+					value={recipe.author}
+				/>
+
+				<Text style={styles.label}>Ingrédients</Text>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setRecipeIngredients(value)}
+					value={recipe.ingredients[0]}
+				/>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setRecipeIngredients(value)}
+					value={recipe.ingredients[0]}
+				/>
+				<Text style={styles.label}>Instructions</Text>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setDirection(value)}
+					value={recipe.direction}
+				/>
+
+				<Text style={styles.label}>Tags</Text>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setRecipeTags(value)}
+					value={recipe.tags[0]}
+				/>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setRecipeTags(value)}
+					value={recipe.tags[1]}
+				/>
+				<TextInput
+					style={styles.input}
+					onChangeText={(value) => setRecipeTags(value)}
+					value={recipe.tags[2]}
+				/>
+
+				<Text style={styles.label}>Publication</Text>
+				{/* inserer toggle */}
+
+				<TouchableOpacity
+					style={styles.button}
+					title="Valider le formulaire"
+					onPress={() => handleSubmitForm()}
+				><Text style={styles.text}>Valider le formulaire</Text></TouchableOpacity>
+			</ScrollView>
+
+
+			<View
+				style={{
+					height: props.bottomTabHeight,
+					backgroundColor: "#f5f6fa",
+					display: "flex",
+					justifyContent: "center",
+				}}
+			>
 				<View
 					style={{
 						height: props.bottomTabHeight,
@@ -54,32 +166,24 @@ function FormScreen(props) {
 						justifyContent: "center",
 					}}
 				>
-					<View
-						style={{
-							height: props.bottomTabHeight,
-							backgroundColor: "#f5f6fa",
-							display: "flex",
-							justifyContent: "center",
-						}}
+					<TouchableOpacity
+						style={{}}
+						onPress={() => navigation.goBack()}
 					>
-						<TouchableOpacity
-							style={{}}
-							onPress={() => navigation.goBack()}
-						>
-							<MaterialCommunityIcons
-								name="arrow-left"
-								size={28}
-								color="#2f3542"
-								style={{
-									paddingLeft: 20,
-									paddingRight: 20,
-									paddingTop: 10,
-									paddingBottom: 10,
-									zIndex: 1,
-								}}
-							/>
-						</TouchableOpacity>
-					</View>
+						<MaterialCommunityIcons
+							name="arrow-left"
+							size={28}
+							color="#2f3542"
+							style={{
+								paddingLeft: 20,
+								paddingRight: 20,
+								paddingTop: 10,
+								paddingBottom: 10,
+								zIndex: 1,
+							}}
+						/>
+					</TouchableOpacity>
+					{/* ------------------------------------Fin du formulaire */}
 				</View>
 			</View>
 		</View>
@@ -119,4 +223,21 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "space-between",
 	},
+	button: {
+		backgroundColor: "#2F3542",
+		alignItems: "center",
+		padding: 10,
+	},
+	text: {
+		color: "#f5f6fa",
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+	},
+	label: {
+
+	}
 });
