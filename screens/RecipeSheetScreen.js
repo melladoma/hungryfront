d3b3e863a2f01b36f7624f51c6603feea7cf40c2
Marine Mellado <marine.mellado@gmail.com>
2022-08-09 17:29:52 +0200

    semainier dynamique

--- screens/RecipeSheetScreen.js
+++ screens/RecipeSheetScreen.js
@@ -266,7 +266,11 @@ function RecipeSheetScreen(props) {
 	var handleCalendarAdd = async (date, token, recipe) => {
 		// console.log(recipeId, date, token)
 		let calendarObj = {
-			date,
+			date: date.toLocaleString('fr-FR', {
+				year: 'numeric',
+				month: 'numeric',
+				day: 'numeric',
+			}),
 			recipe: recipe._id,
 			token
 		}
